e7f7ba052fcfcfd33c71f1dd2926fcd83dbc0c5b
Muhhae <haekal.muhamat@gmail.com>
2024-01-03 21:46:38 +0700

    set up project, air doesnt work

---
+++ cmd/app/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/a-h/templ"
+	"github.com/muhhae/go-templ-htmx/internal/view"
+)
+
+func main() {
+	component := view.Hello("World")
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("internal/static"))))
+
+	http.Handle("/", templ.Handler(component))
+
+	fmt.Println("Listening on http://localhost:3000")
+	http.ListenAndServe("localhost:3000", nil)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/muhhae/go-templ-htmx
+
+go 1.21.3
+
+require github.com/a-h/templ v0.2.513
